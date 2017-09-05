78321210b648d0bf86ec9928ba928d6b3c2a6433
Vladimir Kolesnik <vkolesni@gmail.com>
2017-09-05 21:30:14 +0300

    * Implemented sdome pid file parsing

--- src/pidstats.py
+++ src/pidstats.py
@@ -11,28 +11,24 @@ class PidStats:
         self.observer.raw_values[self.my_metric_key] = dict()
 
     def calculate_values(self, index):
-        self.observer.raw_values[self.my_metric_key][index] = {"process": {"test": 10}}
-        pid_stats = self.generate_counters(index)
-        print(pid_stats['3358'])
-        exit()
+        self.observer.raw_values[self.my_metric_key][index] = self.generate_counters(index)
 
     def generate_counters(self, index):
         counters = dict()
         for pid in self.observer.file_content[index]['pid']:
             counters[pid] = dict()
             for pid_filename in self.observer.pid_file_list:
-                counters[pid] = self.parse_pid_filename(index, pid, pid_filename)
+                counters[pid][pid_filename] = self.parse_pid_filename(index, pid, pid_filename)
+
         return counters
 
     def parse_pid_filename(self, index, pid, pid_filename):
         proc_dict = dict()
-        proc_dict[pid_filename] = dict()
 
         for line in self.observer.file_content[index]['pid'][pid][pid_filename]:
-            proc_dict[pid_filename][line.strip().split()[0]] = ''.join(line.strip().split()[1:])
+            proc_dict[line.strip().split()[0].replace(":", "")] = ''.join(line.strip().split()[1:])
 
         return proc_dict
-
 
 if __name__ == '__main__':
     pass
