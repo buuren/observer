ea275c08a5b5a21e14495592fd2d384ea69f0546
Vladimir Kolesnik <vkolesni@gmail.com>
2017-09-11 22:41:30 +0300

    Improvements to parsing statm, smaps, maps, io, status files

--- src/pidstats.py
+++ src/pidstats.py
@@ -6,7 +6,9 @@ class PidStats:
     def __init__(self, observer):
         self.observer = observer
         self.my_metric_key = "pid_data"
-        self.my_file_list = ["io", "status", "maps", "smaps", "statm"]
+        # "maps",
+        self.my_file_list = ["io", "status", "smaps", "statm"]
+        #self.my_file_list = ["io", "status", "maps", "smaps", "statm"]
         self.observer.proc_file_dictionary.append(self.my_file_list)
 
         self.observer.calculated_values[self.my_metric_key] = dict()
@@ -17,25 +19,97 @@ class PidStats:
         self.keep_pids = dict()
 
     def calculate_values(self, index):
-        self.observer.raw_values[self.my_metric_key][index] = self.generate_counters(index)
+        # Parse PID files
+        pid_stats = dict()
+        pid_stats = self.generate_counters(index)
+        calculations = dict()
+        # self.observer.raw_values[self.my_metric_key][index] = self.generate_counters(index)
+
+        # Start calculations
+        exit()
+        self.observer.raw_values[self.my_metric_key][index] = calculations
 
     def generate_counters(self, index):
-        counters = dict()
+        pid_stats = dict()
+
         for pid in self.keep_pids[index]:
-            counters[pid] = dict()
-        #for pid_filename in self.observer.file_content[index][self.my_metric_key]:
-            for pid_filename in self.my_file_list:
-                counters[pid][pid_filename] = self.parse_pid_filename(index, pid, '/proc/%s/%s' % (pid, pid_filename))
+            pid_stats[pid] = dict()
+            pid_stats[pid]["io"] = self.parse_io(index, "/proc/%s/io" % pid)
+            pid_stats[pid]["status"] = self.parse_status(index, "/proc/%s/status" % pid)
+            pid_stats[pid]["smaps"] = self.parse_smaps(index, "/proc/%s/smaps" % pid)
+            pid_stats[pid]["statm"] = self.parse_statm(index, "/proc/%s/statm" % pid)
 
-        return counters
+        return pid_stats
 
-    def parse_pid_filename(self, index, pid, pid_filename):
-        proc_dict = dict()
+    def parse_io(self, index, pid_filename):
+        return {
+            line.strip().split()[0].replace(":", ""): ''.join(line.strip().replace('kB', '').split()[1:])
+            for line in self.observer.file_content[index][self.my_metric_key][pid_filename]
+        }
 
-        for line in self.observer.file_content[index][self.my_metric_key][pid_filename]:
-            proc_dict[line.strip().split()[0].replace(":", "")] = ''.join(line.strip().split()[1:])
+    def parse_status(self, index, pid_filename):
+        return {
+            line.strip().split()[0].replace(":", ""): ''.join(line.strip().replace('kB', '').split()[1:])
+            for line in self.observer.file_content[index][self.my_metric_key][pid_filename]
+        }
 
-        return proc_dict
+    def parse_smaps(self, index, pid_filename):
+        file_content = self.observer.file_content[index][self.my_metric_key][pid_filename]
+        chunked_file = [file_content[i:i + 21] for i in range(0, len(file_content), 21)]
+
+        return {
+            chunk[0]: {
+                    chunk[n].split()[0]: chunk[n].split()[1:]
+            } for n in range(1, 21)
+            for chunk in chunked_file
+        }
+
+    def parse_statm(self, index, pid_filename):
+        file_content = self.observer.file_content[index][self.my_metric_key][pid_filename]
+
+        size, resident, shared, text, lib, data, dt = file_content[0].split()
+        del self, index, pid_filename, file_content
+
+        return {
+            k: v
+            for k, v in locals().items()
+        }
+
+
+    def get_process_memory_stats(self, index):
+        """
+        based on  statm parser:
+
+        size       (1) total program size (same as VmSize in /proc/[pid]/status)
+        resident   (2) resident set size (same as VmRSS in /proc/[pid]/status)
+        shared     (3) number of resident shared pages (i.e., backed by a file) (same as RssFile+RssShmem in /proc/[pid]/status)
+        text       (4) text (code)
+        lib        (5) library (unused since Linux 2.6; always 0)
+        data       (6) data + stack
+        dt         (7) dirty pages (unused since Linux 2.6; always 0)
+
+        VmSize in status
+        Shareds
+        mem_ids
+        swaps
+        shared_swaps
+        http://man7.org/linux/man-pages/man5/proc.5.html
+        """
+        #dict_keys(['io', 'status', 'maps', 'smaps', 'statm'])
+
+        print(self.observer.raw_values[self.my_metric_key][index]['3334']['smaps'])
+        exit()
+
+        shared_mem = sum(
+            [
+                int(line.split()[1])
+                for line in self.observer.raw_values[self.my_metric_key][index]
+            ]
+        )
+
+        private_mem = sum(
+            [int(line.split()[1]) for line in self.observer.raw_values[self.my_metric_key][index]]
+        )
 
     def return_proc_location(self, index):
         pid_list = self.return_pid_list()
