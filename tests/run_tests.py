ba89354df57ae0ea55b215ebdf18fe64fbf8d703
Vladimir Kolesnik <vkolesni@gmail.com>
2017-09-27 19:59:48 +0300

    Structural changes

---
+++ tests/run_tests.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import time
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from
+print(sys.path)
+
+start = time.time()
+_sleep = 1
+_count = 3
+o = Observer(sleep=_sleep, count=_count)
+o.generate_calculated_values()
+print("Finished calculations in [%s] seconds" % (time.time() - start - (_sleep * (_count - 1))))
+
+exit(0)
